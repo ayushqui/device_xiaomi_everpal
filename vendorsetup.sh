51c1585bace173190b42bb37529cf49974875982
ayushqui <113664933+ayushqui@users.noreply.github.com>
2024-09-18 19:56:08 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,7 +7,7 @@ rm -rf device/mediatek/sepolicy_vndr && git clone https://github.com/MTK-DM-810-
 #hw mtk
 rm -rf hardware/mediatek && git clone https://github.com/MTK-DM-810-UNIFIED/android_hardware_mediatek -b lineage-20-foss hardware/mediatek
 #hw ximi
-rm -rfhardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+rm -rf hardware/xiaomi && git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 #miui cam
 git clone https://gitlab.com/mmtrt/vendor_xiaomi_camera-everpal.git -b thirteen vendor/xiaomi/camera
 #viperfx
