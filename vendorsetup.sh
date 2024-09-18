c71d462ae0ffdef70c3404c727926870501f7e77
ayushqui <113664933+ayushqui@users.noreply.github.com>
2024-09-18 19:21:07 +0530

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+#vendor tree
+git clone https://gitlab.com/mmtrt/vendor_xiaomi_everpal -b sepolicy vendor/xiaomi/everpal
+#kernel
+git clone https://gitlab.com/mmtrt/device_xiaomi_everpal-kernel -b thirteen device/xiaomi/everpal-kernel
+#sepolicy
+git clone https://github.com/MTK-DM-810-UNIFIED/android_device_mediatek_sepolicy_vndr -b lineage-20 device/mediatek/sepolicy_vndr
+#hw mtk
+git clone https://github.com/MTK-DM-810-UNIFIED/android_hardware_mediatek -b lineage-20-foss hardware/mediatek
+#hw ximi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+#miui cam
+git clone https://gitlab.com/mmtrt/vendor_xiaomi_camera-everpal.git -b thirteen vendor/xiaomi/camera
+#viperfx
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
